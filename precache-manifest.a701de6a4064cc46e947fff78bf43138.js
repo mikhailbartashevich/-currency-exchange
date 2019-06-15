2ce034469852de33f324af4c063cbeb607213b5c
Mikhail <mihailbarashevich@gmail.com>
2019-06-15 13:06:58 +0200

    Updates

---
+++ precache-manifest.a701de6a4064cc46e947fff78bf43138.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771bc8deccffdd1198c31527c9e6636f",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "f0e9da393c0ae8387d5b",
+    "url": "/currency-exchange/static/css/main.009338b9.chunk.css"
+  },
+  {
+    "revision": "0fd3de5abc646a97b9c5",
+    "url": "/currency-exchange/static/js/2.53eac7c1.chunk.js"
+  },
+  {
+    "revision": "f0e9da393c0ae8387d5b",
+    "url": "/currency-exchange/static/js/main.42a9e3ec.chunk.js"
+  },
+  {
+    "revision": "d7001c845ee7f1f64c01",
+    "url": "/currency-exchange/static/js/runtime~main.802ca58e.js"
+  }
+]);
--- src/store/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux'
-import { currencyExchange } from './currencyExchange'
-
-export default combineReducers({
-  currencyExchange,
-})
