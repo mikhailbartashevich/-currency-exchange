11ee793d17d6058b73e4ae2e247eaa894556c1ee
Mikhail <mihailbarashevich@gmail.com>
2019-06-16 09:43:55 +0200

    code cleanup

--- src/index.js
+++ src/index.js
@@ -9,8 +9,6 @@ import rootReducer from './store/reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 
-console.log(process.env.NODE_ENV);
-
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
@@ -27,7 +25,4 @@ render(
   document.getElementById('root'),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
