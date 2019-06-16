6e9bdfc8f58fdd0547564860c51b9bad7074e88e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-16 14:39:40 +0000

    Deploy mikhailbartashevich/currency-exchange to github.com/mikhailbartashevich/currency-exchange.git:gh-pages

---
+++ precache-manifest.e102e7c5b4c1406f1e9906424ebb04e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62876a2ccaba4aa16ab77642f8aa22dc",
+    "url": "/currency-exchange/index.html"
+  },
+  {
+    "revision": "2d240d62c72e6a39884f",
+    "url": "/currency-exchange/static/css/main.09a886cc.chunk.css"
+  },
+  {
+    "revision": "b27fe0a2b1118aeb2bf4",
+    "url": "/currency-exchange/static/js/2.9e756ada.chunk.js"
+  },
+  {
+    "revision": "2d240d62c72e6a39884f",
+    "url": "/currency-exchange/static/js/main.d9947f4f.chunk.js"
+  },
+  {
+    "revision": "d7001c845ee7f1f64c01",
+    "url": "/currency-exchange/static/js/runtime~main.802ca58e.js"
+  }
+]);
